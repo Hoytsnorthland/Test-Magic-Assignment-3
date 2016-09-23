482945eb8bc87b707ca1d5f295828af9dd7efc8e
Hoytsnorthland <vimalchandrach@yahoo.com>
2016-09-23 11:04:43 +1000

    Add files via upload

--- ITC515_NewLibrary/src/library/interfaces/daos/IMemberHelper.java
+++ ITC515_NewLibrary/src/library/interfaces/daos/IMemberHelper.java
@@ -5,8 +5,5 @@ import library.interfaces.entities.IMember;
 public interface IMemberHelper {
 	
 	public IMember makeMember(String firstName, String lastName, String contactPhone, String emailAddress, int id);
-{
-	return new Mmber(firstName, lastName, contactPhone, emailAddress, id);
-}
 
 }
